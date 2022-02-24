2c2cb661fc965e84bffa44ae00d2e522359a2e62
BurwitzBlagger <99707350+BurwitzBlagger@users.noreply.github.com>
2022-02-24 17:28:02 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+             printf("We're no strangers to love\n");
+             Sleep(3000);
+             printf("You know the rules, and so do I\n");
+             Sleep(3500);
+             printf("A full commitment's what I'm thinking of\n");
+             Sleep(3600);
+             printf("You won't get this from any other guy\n");
+             Sleep(3700);
+             printf("I.....");
+             Sleep(2700);
+             printf("Just wanna tell you how I'm feeling\n");
+             Sleep(3200);
+             printf("Gotta make you");
+             Sleep(2000);
+             printf(" understand\n");
+             Sleep(1900);
+             printf("Never gonna give you up\n");
+             Sleep(2500);
+             printf("Never gonna let you down\n");
+             Sleep(3000);
+             printf("Never gonna run around and desert you\n");
+             Sleep(4000);
+             printf("Never gonna make you cry\n");
+             Sleep(3000);
+             printf("Never gonna say goodbye\n");
+             Sleep(2800);
+             printf("Never gonna tell a lie and hurt you\n");
+             Sleep(4000);
+             printf("We've known each other");
+             Sleep(2800);
+             printf(" for so long\n");
+             Sleep(2800);
+             printf("Your heart's been aching but");
+             Sleep(900);
+             printf(" you're too shy to say it\n");
+             Sleep(4000);
+             printf("Inside we both know what's been");
+             Sleep(1000);
+             printf(" going on\n");
+             Sleep(2000);
+             printf("We know the game and we're");
+             Sleep(1000);
+               printf(" gonna play it\n");
+             Sleep(2000);
+               printf("And.......");
+             Sleep(2000);
+             printf("if you ask me how I'm feeling\n");
+             Sleep(3500);
+             printf("Don't tell me you're too");
+             Sleep(900);
+             printf(" blind to see\n");
+             Sleep(1000);
+             printf("Never gonna give you up\n");
+             Sleep(2500);
+             printf("Never gonna let you down\n");
+             Sleep(3000);
+             printf("Never gonna run around and desert you\n");
+             Sleep(4000);
+             printf("Never gonna make you cry\n");
+             Sleep(3000);
+             printf("Never gonna say goodbye\n");
+             Sleep(2800);
+             printf("Never gonna tell a lie and hurt you\n");
+             Sleep(4000);
+
+
+
+             system("PAUSE");
+
+
+}
